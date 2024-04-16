7b730febddeb08bb0e05517160e9801d2e3c4a4e
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-04-15 19:40:09 -0500

    making changes in the node group name

--- main.tf
+++ main.tf
@@ -23,12 +23,6 @@ resource "aws_eks_cluster" "main" {
   vpc_config {
     subnet_ids = var.subnet_ids
   }
-
-
-  depends_on = [
-    aws_iam_role_policy_attachment.main-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.main-AmazonEKSVPCResourceController,
-  ]
 }
 
 
