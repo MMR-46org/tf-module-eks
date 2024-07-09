2c30d80c1e29c400761dd878bb38d063f1c24c60
MMR-46org <madhanmohanreddyeeda@gmail.com>
2024-07-09 14:34:58 -0500

    creation of oidc identifier for pods to gain access through the aws environment

--- main.tf
+++ main.tf
@@ -150,3 +150,15 @@ resource "aws_iam_role_policy_attachment" "extra-policy-attach" {
   policy_arn = aws_iam_policy.node-extra-policy.arn
   role       = aws_iam_role.node.name
 }
+
+
+
+
+
+resource "aws_iam_openid_connect_provider" "default" {
+
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+  client_id_list = [ "sts.amazonaws.com" ]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"]
+
+}
